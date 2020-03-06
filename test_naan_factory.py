603725a0d1dbd82721c0f05af99ade008dd6f89f
Elliot Harris <elliotharris@tcadmins-MacBook-Air.local>
2020-03-06 11:17:44 +0000

    test completed for user story 2

--- test_naan_factory.py
+++ test_naan_factory.py
@@ -4,14 +4,14 @@
 from naan_factory_functions import  *
 
 
-# # As a user, I can use the make_dough with water and flour to make dough.
-# print("Calling make_dough with water and flour, expect 'dough' as a result")
-# print(make_dough("water", "flour") == "dough")
-# print("got:", make_dough("water", "flour"))
-#
-# print("Calling make_dough with water and cement, expect 'not dough' as a result")
-# print(make_dough("water", "cement") == "not dough")
-# print("got:", make_dough("water", "cement"))
+# As a user, I can use the make_dough with water and flour to make dough.
+print("Calling make_dough with water and flour, expect 'dough' as a result")
+print(make_dough("water", "flour") == "dough")
+print("got:", make_dough("water", "flour"))
+
+print("Calling make_dough with water and cement, expect 'not dough' as a result")
+print(make_dough("water", "cement") == "not dough")
+print("got:", make_dough("water", "cement"))
 
 
 #2
@@ -20,3 +20,6 @@ from naan_factory_functions import  *
 print("Calling bake_dough with dough to get naan, expect 'naan' as a result")
 print(bake_dough("dough") == "naan")
 print("got: ", bake_dough("dough"))
+
+#3
+#As a user, I can user the run_factory with water and flour and get naan.
